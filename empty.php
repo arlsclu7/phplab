66b2e6e937309758acb6bdd303d28d2035e1e433
arlsclu <arlsclu@yahoo.com>
2020-04-03 14:38:30 +0800

    modify empty

--- empty.php
+++ empty.php
@@ -6,10 +6,11 @@ $rst =  !($a??1)?:0;
 $rst = isset($b)?'111':'noexist';
 $rst = $d??'11';
 /**
- * exp??exp2 =>  isset($ext)  true  $ext  exp2 ..
- * func(ext)?:    如果ext为true，那么   func(ext).?:之间有值则为该值
+ * exp1??exp2 =>  ??中间无表达式  exp1 为isset时，输出exp1
+ * exp1?[exp2]:exp3   使用empty判断exp1, 如果exp1为true，exp2为空时显示exp1的值（可能为boolean），exp2非空时显示exp2的值
  */
-
+$e = '0.0';
+$rst = $e?:'yyy';
 var_dump( $rst);
 exit();
 
