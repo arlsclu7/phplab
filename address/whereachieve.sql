7fc8bf5163d9b3818b09f677df3a33e162f7c131
arlsclu <arlsclu@yahoo.com>
2020-04-07 11:40:38 +0800

    modfiy

---
+++ address/whereachieve.sql
@@ -0,0 +1,24 @@
+select  distinct concat(achiev_city_id,',')  from t_city_achiev_city where achiev_city_name in  (
+'南京',
+'嘉兴',
+'湖州',
+'绍兴',
+'金华',
+'台州',
+'沈阳',
+'兰州',
+'南宁',
+'银川'
+)
+
+
+109,
+125,
+126,
+127,
+128,
+131,
+253,
+336,
+358,
+71
