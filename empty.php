893ee774e19c236c6b94404719cc6f1af9286a35
arlsclu <arlsclu@yahoo.com>
2020-05-27 16:14:49 +0800

    modify

--- empty.php
+++ empty.php
@@ -1,27 +1,83 @@
 <?php
-$b = 10;
-$c =100;
-$d= null;
-$rst =  !($a??1)?:0;
-$rst = isset($b)?'111':'noexist';
-$rst = $d??'11';
-/**
- * exp1??exp2 =>  ??中间无表达式  exp1 为isset时，输出exp1
- * exp1?[exp2]:exp3   使用empty判断exp1, 如果exp1为true，exp2为空时显示exp1的值（可能为boolean），exp2非空时显示exp2的值
- */
-$e = '0.0';
-$rst = $e?:'yyy';
-var_dump( $rst);
-exit();
 
-
-
+#### That means empty() is essentially the concise equivalent to !isset($var) || $var == false.
+// When converting to boolean, the following values are considered FALSE:
+// the boolean FALSE itself
+// the integers 0 and -0 (zero)
+// the floats 0.0 and -0.0 (zero)
+// the empty string, and the string "0"
+// an array with zero elements
+// the special type NULL (including unset variables)
+// SimpleXML objects created from empty tags
 $arr = [
-    new \SimpleXML,0,0.0,-0.0,'','0','0.0','-0.0',[],new \stdClass
-];
 
-foreach($arr as $v) {
-    $rst[] = [$v,empty($v)?'y':'n',gettype($v)];
+    ##integer
+    0,
+    1,
+    -0,
+    ##float
+    0.0,
+    -0.0,
+    0.000,
+
+
+    ##boolean
+    true,
+    false,
+    ##string
+    '',
+    '0',
+    '-0.0',
+    '0.0000',
+
+    ##array
+    array(),
+    ##object
+    new \stdClass(),
+
+
+    ##Null
+    null,
+
+
+];
+/**
+ * [integer, 0
+ * float]  0.0 0.00  -0.0
+ * 
+ * [boolean, 
+ * string] '','0','0.00',' '
+ * 
+ * [array,  
+ *  object]
+ * 
+ * [null,
+ * resource]
+ * 
+ * 
+ * 
+ */
+
+$arr = array_combine(range('a','o'),$arr);
+$true = [];
+$false = [];
+$empty = [];
+$not_empty = [];
+foreach($arr as $k=>$v) {
+    if($v) {
+        $true[] = [$k=>$v];
+    } else {
+        $false[] = [$k=>$v];
+    }
+    if(empty($v)) {
+        $empty[] = [$k=>$v];
+    } else {
+        $not_empty[] = [$k=>$v];
+    }
 }
 
-var_export($rst);
+var_dump($true);
+var_dump($false);
+var_dump($empty);
+var_dump($not_empty);
+
